41fb79bdaa76d9e31375bdcaf492b6b6f29cd794
yasyamanoi <yasyamanoi@gmail.com>
2024-06-10 16:04:21 +0900

    サイドバー付き雛形の追加

--- sources/common/contents_nodes.php
+++ sources/common/contents_nodes.php
@@ -57,6 +57,7 @@ class cheader extends cnode {
 <li class="nav-item"><a href="member_list.php" class="nav-link link-success">メンバー管理</a></li>
 <li class="nav-item"><a href="member_list_custom.php" class="nav-link link-success">メンバー管理（カスタムノード）</a></li>
 <li class="nav-item"><a href="hinagata.php" class="nav-link link-success">雛形ファイル</a></li>
+<li class="nav-item"><a href="hinagata2.php" class="nav-link link-success">雛形ファイル（サイドバー付き）</a></li>
 </ul>
 </header>
 END_BLOCK;
@@ -72,6 +73,111 @@ END_BLOCK;
 		parent::__destruct();
 	}
 }
+
+//--------------------------------------------------------------------------------------
+///	サイドバー付きヘッダノード
+//--------------------------------------------------------------------------------------
+class cside_header extends cnode {
+	//--------------------------------------------------------------------------------------
+	/*!
+	@brief	コンストラクタ
+	*/
+	//--------------------------------------------------------------------------------------
+	public function __construct() {
+		//親クラスのコンストラクタを呼ぶ
+		parent::__construct();
+	}
+	//--------------------------------------------------------------------------------------
+	/*!
+	@brief	構築時の処理(継承して使用)
+	@return	なし
+	*/
+	//--------------------------------------------------------------------------------------
+	public function create(){
+	}
+	//--------------------------------------------------------------------------------------
+	/*!
+	@brief  表示(継承して使用)
+	@return なし
+	*/
+	//--------------------------------------------------------------------------------------
+	public function display(){
+		$echo_str = <<< END_BLOCK
+
+<!doctype html>
+<html lang="ja">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>サンプルサイト</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+<!-- 全体コンテナ　-->
+<div class="container-fluid">
+<header class="navbar sticky-top bg-secondary-subtle flex-md-nowrap p-0 shadow" >
+<a class="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6 text-dark" href="#">サイト名</a>
+</header>
+	<!-- 行　-->
+	<div class="row">
+		<div class="sidebar border col-md-3 col-lg-2 p-1 bg-body-tertiary">
+			<ul class="nav flex-column">
+				<li class="nav-item">
+					<a class="nav-link d-flex align-items-center gap-2 active" aria-current="page" href="index.php">
+						<span class="bi"></span>
+						メインメニュー
+					</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link d-flex align-items-center gap-2" href="prefecture_list.php">
+						<span class="bi"></span>
+						都道府県管理
+					</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link d-flex align-items-center gap-2" href="member_list.php">
+						<span class="bi"></span>
+						メンバー管理
+					</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link d-flex align-items-center gap-2" href="member_list_custom.php">
+						<span class="bi"></span>
+						メンバー管理（カスタムノード）
+					</a>
+				</li>
+			</ul>
+			<hr class="my-1">
+			<ul class="nav flex-column mb-auto">
+				<li class="nav-item">
+					<a class="nav-link d-flex align-items-center gap-2" href="hinagata.php">
+						<span class="bi"></span>
+						雛形ファイル
+					</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link d-flex align-items-center gap-2" href="hinagata2.php">
+						<span class="bi"></span>
+						雛形ファイル（サイドバー付き）
+					</a>
+				</li>
+			</ul>
+		</div>
+END_BLOCK;
+		echo $echo_str;
+	}
+	//--------------------------------------------------------------------------------------
+	/*!
+	@brief	デストラクタ
+	*/
+	//--------------------------------------------------------------------------------------
+	public function __destruct(){
+		//親クラスのデストラクタを呼ぶ
+		parent::__destruct();
+	}
+}
+
 
 //--------------------------------------------------------------------------------------
 ///	フッターノード
@@ -109,6 +215,65 @@ class cfooter extends cnode {
 </div>
 <!-- /全体コンテナ　-->
 <div class="b-divider"></div>
+<script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js" integrity="sha384-fbbOQedDUMZZ5KreZpsbe1LCZPVmfTnH7ois6mU1QK+m14rQ1l2bGBq41eYeM/fS" crossorigin="anonymous"></script>
+<script src="js/scripts.js"></script>
+</body>
+</html>
+END_BLOCK;
+		echo $echo_str;
+	}
+	//--------------------------------------------------------------------------------------
+	/*!
+	@brief	デストラクタ
+	*/
+	//--------------------------------------------------------------------------------------
+	public function __destruct(){
+		//親クラスのデストラクタを呼ぶ
+		parent::__destruct();
+	}
+}
+
+
+//--------------------------------------------------------------------------------------
+///	サイドバー付きフッターノード
+//--------------------------------------------------------------------------------------
+class cside_footer extends cnode {
+	//--------------------------------------------------------------------------------------
+	/*!
+	@brief	コンストラクタ
+	*/
+	//--------------------------------------------------------------------------------------
+	public function __construct() {
+		//親クラスのコンストラクタを呼ぶ
+		parent::__construct();
+	}
+	//--------------------------------------------------------------------------------------
+	/*!
+	@brief	構築時の処理(継承して使用)
+	@return	なし
+	*/
+	//--------------------------------------------------------------------------------------
+	public function create(){
+	}
+	//--------------------------------------------------------------------------------------
+	/*!
+	@brief  表示(継承して使用)
+	@return なし
+	*/
+	//--------------------------------------------------------------------------------------
+	public function display(){
+		$echo_str = <<< END_BLOCK
+
+		<footer class="py-3 my-4 border-dark border-top">
+			<p class="text-center text-body-secondary">&copy; 2024 サイト名</p>
+		</footer>
+	</div>
+	<!--/ 行　-->
+</div>
+<!-- /全体コンテナ　-->
+	<div class="b-divider"></div>
 <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js" integrity="sha384-fbbOQedDUMZZ5KreZpsbe1LCZPVmfTnH7ois6mU1QK+m14rQ1l2bGBq41eYeM/fS" crossorigin="anonymous"></script>
